03a499be2afae0440939881edd5c259cbf0167d8
Prince Kumar <hello@onemoreprince.com>
2025-02-08 15:25:44 +0530

    Fixed relative URLs

--- assets/js/main.js
+++ assets/js/main.js
@@ -136,7 +136,7 @@ function attachStockInFormListener() {
 // --- Service Worker Registration for PWA ---
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js')
+        navigator.serviceWorker.register('./service-worker.js')
             .then(registration => {
                 console.log('Service Worker registered with scope: ', registration.scope);
             }).catch(err => {
